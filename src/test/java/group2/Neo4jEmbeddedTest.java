6e8b17a0e5822fc0a9a3ad5d71d9298378772204
venkatesh reddy <venkateshgunda@gmail.com>
2022-04-25 10:15:23 -0400

    sample maven project

---
+++ src/test/java/group2/Neo4jEmbeddedTest.java
@@ -0,0 +1,28 @@
+package group2;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.neo4j.test.EmbeddedDatabaseRule;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Neo4jEmbeddedTest {
+
+    @Rule
+    public EmbeddedDatabaseRule embeddedDatabaseRule = new EmbeddedDatabaseRule();
+
+    private Neo4jEmbeddedExample neo4jEmbeddedExample;
+
+    @Before
+    public void retrieves_service() {
+        neo4jEmbeddedExample = new Neo4jEmbeddedExample(embeddedDatabaseRule.getGraphDatabaseService());
+    }
+
+    @Test
+    public void says_hello_world() {
+        String helloWorld = neo4jEmbeddedExample.helloWorldTraversal();
+
+        assertThat(helloWorld).isEqualTo("Hello world");
+    }
+}
